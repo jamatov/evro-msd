5c747b556d580771e77254413ac3d4de36bba9e7
jamatov <mirabdullajamatov@gmail.com>
2023-12-01 19:46:35 +0500

    call btn aded

---
+++ js/app.js
@@ -0,0 +1,41 @@
+function sendMessage() {
+  const BOT_TOKEN = "6381726609:AAHVJrcq3FJQQ6wyudsHugXesBkIU7kr8GQ";
+  const CHAT_ID = "-1002130721123";
+
+  const name = document.getElementById("name").value;
+  const phoneNumber = document.getElementById("phone").value;
+  const companyName = document.getElementById("company").value;
+
+  let message =
+    "isim: " +
+    name +
+    ".\nXabar: " +
+    companyName +
+    ".\nTelefon nomer: " +
+    
+    phoneNumber;
+
+  let url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage";
+  let data = {
+    chat_id: CHAT_ID,
+    text: message,
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      // window.location.href = "../confirm.html";
+      alert("Завершено успешно. Дождитесь обратного звонка!");
+      document.getElementById("name").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("company").value = "";
+    })
+    .catch((error) => {
+      alert("Failed to submit feedback. Please try again later.");
+    });
+}
